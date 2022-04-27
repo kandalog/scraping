31451e7f44c8ff97db8766fef264bf9de62d4885
kandalog <asuasu.tee@gmail.com>
2022-04-27 20:17:44 +0900

    ログインまで自動化

--- index.py
+++ index.py
@@ -1,5 +1,26 @@
 from selenium import webdriver
 from time import sleep
-# sleep(3)
-print('HERE')
-browser = webdriver.Chrome()
+
+browser = webdriver.Chrome() # ブラウザ起動
+
+browser.get('https://scraping-for-beginner.herokuapp.com/login_page') # urlにアクセス
+sleep(3)
+
+# 操作の大前提（1:どの場所に 2:どんな処理を行いたいか）
+
+# ログインする
+elem_username = browser.find_element_by_id('username') # 要素を取得
+elem_username.send_keys('imanishi') # 文字を入力
+
+elem_password = browser.find_element_by_id('password')
+elem_password.send_keys('kohei')
+
+login_button = browser.find_element_by_id('login-btn')
+sleep(1)
+login_button.click() # クリックする
+
+
+
+
+sleep(5)
+browser.quit() # Chromeを閉じる
